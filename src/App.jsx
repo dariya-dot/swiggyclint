50c87307baa95b69bff152b3635282d55bb06d34
Dariya Hussein Kondapalli <dariya.k007@gmail.com>
2025-01-16 19:43:08 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 import "./App.css";
 import Landingpage from "./swiggyclint/pages/Landingpage";
-import { HashRouter as Route, Routes, Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Products from "./swiggyclint/compnents/Products";
 
 const App = () => {
   return (
     <div>
-      <Router>
+      
         <Routes>
           <Route path="/" element={<Landingpage />} />
           <Route path="/product/:firmId/:firmName" element={<Products />} />
         </Routes>
-      </Router>
+      
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Harshrouter, HashRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {  HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <HashRouter>
-       <StrictMode>
-    <App />
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
-  </HashRouter>
- 
-)
+);
