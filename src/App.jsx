1c6c653afd916bc173f405dfde07416eb18ea0ae
Dariya Hussein Kondapalli <dariya.k007@gmail.com>
2025-01-16 19:36:04 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import './App.css'
-import Landingpage from './swiggyclint/pages/Landingpage'
-import { Route, Routes } from 'react-router-dom'
-import Products from './swiggyclint/compnents/Products'
+import "./App.css";
+import Landingpage from "./swiggyclint/pages/Landingpage";
+import { HashRouter as Route, Routes, Router } from "react-router-dom";
+import Products from "./swiggyclint/compnents/Products";
 
 const App = () => {
   return (
-    
     <div>
-      <Routes>
-        <Route path='/' element={<Landingpage/>} />
-        <Route path='/product/:firmId/:firmName' element={<Products/>}/>
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/product/:firmId/:firmName" element={<Products />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
