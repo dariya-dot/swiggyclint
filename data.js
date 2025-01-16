820672914bf6bbe98fbb0fa77b2c176c409a058a
Dariya Hussein Kondapalli <dariya.k007@gmail.com>
2025-01-16 17:10:30 +0530

    image path with out public

--- data.js
+++ data.js
@@ -1,46 +1,46 @@
 export const itemData=[
     {
-        item_image:"/public/assets/Image/birya.avif",
+        item_image:"/assets/Image/birya.avif",
         name:"Biryani"
     },
     {
-        item_image: "/public/assets/Image/bujji.avif",
+        item_image: "/assets/Image/bujji.avif",
         name:"Bajji"
     },
     {
-        item_image: "/public/assets/Image/Curd Rice.avif",
+        item_image: "/assets/Image/Curd Rice.avif",
         name:"Curd rice"
     },
     {
-        item_image:"/public/assets/Image/Dosa.avif",
+        item_image:"/assets/Image/Dosa.avif",
         name:"dosa"
     },
     {
-        item_image:"/public/assets/Image/Dryfroot Laddu.avif",
+        item_image:"/assets/Image/Dryfroot Laddu.avif",
         name:"dryfoot laddu"
     },
     {
-        item_image:"/public/assets/Image/idly.avif",
+        item_image:"/assets/Image/idly.avif",
         name:"idly"
     },
     {
-        item_image:"/public/assets/Image/mysoorpak.avif",
+        item_image:"/assets/Image/mysoorpak.avif",
         name:"mysoor pak"
     },
     {
-        item_image:"/public/assets/Image/Poori.avif",
+        item_image:"/assets/Image/Poori.avif",
         name:"poori"
     },
     {
-        item_image:"/public/assets/Image/rice Bowl.avif",
+        item_image:"/assets/Image/rice Bowl.avif",
         name:"rice bowl"
     },
     {
-        item_image:"/public/assets/Image/Tandoori.jpg",
+        item_image:"/assets/Image/Tandoori.jpg",
         name:"tandoori"
     },
     {
-        item_image:"/public/assets/Image/vada.avif",
+        item_image:"/assets/Image/vada.avif",
         name:"vada"
     }
 
